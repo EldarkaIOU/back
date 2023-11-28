d9d9f6cdf28b09d582c9ee1f5995051e7b11f568
mooneex <116973490+yusennn@users.noreply.github.com>
2023-11-28 22:08:46 +0600

    tests

--- django_app/tests.py
+++ django_app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import MagicMock
+from .management.commands.run_telegram_bot import start, show_cars, help_command, echo_all
 
-# Create your tests here.
+
+class TestBot(unittest.TestCase):
+
+    def test_start(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        start(message)
+
+    def test_help(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        help_command(message)
+
+    def test_figures(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        show_cars(message)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
